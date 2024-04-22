ad1774609859c9eba66b03d5a1d0485a91d9a4f9
Pablo Fiallos <159185130+pablo-fiallos-granados@users.noreply.github.com>
2024-04-22 18:49:15 +0000

    90%

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Pablo Fiallos
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+function enterClicked() {
+  // input
+  const temperatureFahrenheit = parseFloat(document.getElementById("temperatureFahrenheit").value)
+  const temperatureCelsius = (temperatureFahrenheit - 32) * 5.0 / 9.0
+  const temperatureCelsiusRounded = temperatureCelsius.toFixed(2)
+  // output
+  document.getElementById("user-info").innerHTML =
+    "temperature = " + temperatureCelsiusRounded + "<sup>o</sup>C"
+}
